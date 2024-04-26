811d8defed9b06b5ad5cb6218d9101282d089591
megjohnson324 <152332094+megjohnson324@users.noreply.github.com>
2024-04-26 00:19:22 -0400

    Update run_repeatmasker.sh

--- run_repeatmasker.sh
+++ run_repeatmasker.sh
@@ -1,14 +1,19 @@
+#This script runs the RepeatMasker program on the gray whale genome file with a specification to output a GFF file to be used in later analysis.
+#RepeatMasker is a program that screens a genome and provides an output of repeats in the genome file. 
 #!/bin/bash
-#SBATCH --job-name=test_repeatmodeler
-#SBATCH -A debug
+#SBATCH --job-name=repeatmasker2
+#SBATCH -A highmem
 #SBATCH -N 1
 #SBATCH -n 64
-#SBATCH -t 00:30:00
-#SBATCH --error=repeatmodeler.err
-#SBATCH --output=repeatmodeler.out
+#SBATCH -t 1-00:00:00
+#SBATCH --error=repeatmodeler2.err
+#SBATCH --output=repeatmodeler2.out
+#SBATCH --mail-user=john2788@purdue.edu
+#SBATCH --mail-type=START,END,FAIL
 
 module --force purge
 ml biocontainers repeatmasker
 
-#trying repeatmasker with gray whale genome
-RepeatMasker -pa 64 -qq -species mammals GCA_028021215.1_mEscRob2.pri_genomic.fna
+#repeatmasker with gray whale genome
+#include -gff if you want a gff file output format
+RepeatMasker -pa 64 -gff -qq -species mammals GCA_028021215.1_mEscRob2.pri_genomic.fna
